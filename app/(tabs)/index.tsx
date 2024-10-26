192bfb488bbf68e0f8a1afe8c21da7f1094c4e6f
arfkan <140435669+arfkan@users.noreply.github.com>
2024-10-26 12:32:40 +0300

    cleanup and dummy data

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { Stack } from 'expo-router';
+import {  View } from 'react-native';
+
+
+
+export default function Home() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Events' }} />
+    </>
+  );
+}
+
